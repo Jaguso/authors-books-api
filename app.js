0f8c114ab390e13073da46c5b07cb8df12e9fd8d
jaguso <jagutzo@gmail.com>
2018-11-30 16:56:06 -0600

    mongoDB and mongoose configuration

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const authorRoutes = require('./api/routes/authors');
+
+mongoose.connect(
+    'mongodb://author:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@cluster0-shard-00-00-t24xm.mongodb.net:27017,cluster0-shard-00-01-t24xm.mongodb.net:27017,cluster0-shard-00-02-t24xm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', 
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(morgan('dev'));
 
