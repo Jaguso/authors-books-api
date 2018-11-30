524cf83246197cdb8ce6aa62cfdd5872e86a93e0
jaguso <jagutzo@gmail.com>
2018-11-30 15:28:20 -0600

    errors configuration

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ const morgan = require('morgan');
 
 const authorRoutes = require('./api/routes/authors');
 
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It works!'
-//     });
-// });
-
+app.use(morgan('dev'));
 
 app.use('/authors', authorRoutes);
 
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+}); 
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
